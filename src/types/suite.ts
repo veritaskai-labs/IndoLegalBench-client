1e8da61ff4bd52639c851fad79daeffa71d249ba
Herdayani Elision Sitio <elisionstherdayani@gmail.com>
2026-09-24 11:53:56 +0700

    test(suites): SCRUM-69 SCRUM-100 add failing test for useSuites

---
+++ src/types/suite.ts
@@ -0,0 +1,7 @@
+import type { components } from "@/lib/generated/api";
+
+export type Suite = components["schemas"]["SuiteRead"];
+export type SuiteStatus = components["schemas"]["SuiteStatus"];
+export type SuitePage = components["schemas"]["Page_SuiteRead_"];
+export type SuiteCreate = components["schemas"]["SuiteCreate"];
+export type SuiteUpdate = components["schemas"]["SuiteUpdate"];
